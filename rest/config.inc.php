6dd9a523e4c630e8c6b041042007e480db3e72e0
Li Liu <Li.L.Liu@newegg.com>
2015-07-30 16:39:19 +0800

    retrieveAttributeSet

--- rest/config.inc.php
+++ rest/config.inc.php
@@ -4,6 +4,7 @@ ini_set('max_execution_time', 2400);
 
 $config = array(
     'debugMode' => false,
+    'ItemNumberStoreDirectory' => 'itemNumbers/',
     'rwListUrl' => 'http://10.1.24.130:3000/ExternalMarketplace/v1/RosewillItem',
     'pimUrlBase' => 'http://rwpim.silksoftware.net/',
     'pimAttributeSetRestPostfix' => 'helloworld'
