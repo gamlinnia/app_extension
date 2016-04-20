85c02e5929b46acc6ba5000f48cc4ccb9d110729
th98 <tim.h.huang@newegg.com>
2016-04-20 09:28:23 +0800

    add getRwProductListAndPostToDev.php

--- getRwProductListAndPostToDev.php
+++ getRwProductListAndPostToDev.php
@@ -1,42 +1,64 @@
 <?php
 require_once 'rest/tools.php';
 
-//GET: http://api.newegg.org/ExternalMarketplace/v1/RosewillItem?format=json&Manufactory=2177&ItemCreationDateFrom=2000-01-16
-$neweggApiUrl = "http://api.newegg.org/ExternalMarketplace/v1/RosewillItem";
-$localApiUrl = "http://10.16.197.90/app_extension/rest/route.php/api/getCombinationInfo";
-$devRestApiUrl = "http://rwdev.buyabs.corp/rest/route.php/api/postProductJsonToLocal";
+function getRwProductList(){
+    $neweggApiUrl = "http://api.newegg.org/ExternalMarketplace/v1/RosewillItem";
 
-$params['format'] = 'json';
-$params['Manufactory'] = '2177';
-$params['ItemCreationDateFrom'] = '2000-01-16';
-$headers =  array('Content-Type: application/json', 'Accept: application/json', 'Token: rosewill');
+    $params['format'] = 'json';
+    $params['Manufactory'] = '2177';
+    $params['ItemCreationDateFrom'] = '2000-01-16';
 
+    $headers =  array('Content-Type: application/json', 'Accept: application/json');
+
+    $response = CallAPI('GET', $neweggApiUrl . parseQueryString($params), $headers);
+
+    return $response["ResultList"];
+}
+
+function getProductInfo($input = array()){
+    $localApiUrl = "http://10.16.197.90/app_extension/rest/route.php/api/getCombinationInfo";
+
+    $headers =  array('Content-Type: application/json', 'Accept: application/json');
+
+    $response = CallAPI('POST', $localApiUrl, $headers, $input);
+
+    return $response;
+}
+
+function postProductInfoToDev($productInfo){
+    $devRestApiUrl = "http://rwdev.buyabs.corp/rest/route.php/api/postProductJsonToLocal";
+
+    $headers =  array('Content-Type: application/json', 'Accept: application/json', 'Token: rosewill');
+
+    $response = CallAPI('POST', $devRestApiUrl, $headers, $productInfo);
+
+    return $response;
+}
+
+function logErrorMessage($input, $response){
+    $log = array();
+    $log['item number'] = $input['ItemNumber'];
+    $log['response'] = $response;
+    $now = new DateTime(null, new DateTimeZone('UTC'));
+    $log['date'] = $now->format('Y-m-d H:i:s');
+    file_put_contents('post.log', json_encode($log) . PHP_EOL, FILE_APPEND);
+}
 // get rosewill product list from newegg
-$response = CallAPI('GET', $neweggApiUrl . parseQueryString($params), $headers);
-
-$rwProductList = $response["ResultList"];
+$rwProductList = getRwProductList();
 if(!empty($rwProductList)) {
     foreach ($rwProductList as $each) {
         $input['ItemNumber'] = $each['ItemNumber'];
         echo $input["ItemNumber"] . PHP_EOL;
         $input['action'] = 'baseinfo';
-        //POST: http://10.16.197.90/app_extension/rest/route.php/api/getCombinationInfo
-        $response = CallAPI('POST', $localApiUrl, $headers, $input);
-        $productInfo = $response;
-        //var_dump($productInfo);
+        $productInfo = getProductInfo($input);
         if (!empty($productInfo)) {
-            $response = CallAPI('POST', $devRestApiUrl, $headers, $productInfo);
+            $response = postProductInfoToDev($productInfo);
             if($response['message'] == 'Success'){
                 echo 'Success' . PHP_EOL;
             }
             else{
                 echo 'Error' . PHP_EOL;
-                $log = array();
-                $log['item number'] = $input['ItemNumber'];
-                $log['response'] = $response;
-                $now = new DateTime(null, new DateTimeZone('UTC'));
-                $log['date'] = $now->format('Y-m-d H:i:s');
-                file_put_contents('post.log', json_encode($log) . PHP_EOL, FILE_APPEND);
+                logErrorMessage($input, $response);
             }
         }
     }
