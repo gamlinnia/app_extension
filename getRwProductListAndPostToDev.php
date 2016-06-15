dd2339e1975958960863def852284da13eaed7f1
Li Liu <Li.L.Liu@newegg.com>
2016-06-15 12:27:35 +0800

    post product info with images to dev

--- getRwProductListAndPostToDev.php
+++ getRwProductListAndPostToDev.php
@@ -35,6 +35,24 @@ function postProductInfoToDev($productInfo){
     return $response;
 }
 
+function getImagesFromIM ($itemNumber) {
+    $IMBaseUrl = 'https://apis.newegg.org';
+//    $imageBase = 'http://images10.newegg.com/productimage';
+//    $imageBase = 'http://10.1.39.209/productimage';
+    $restPostfix = '/content/v1/item/' . $itemNumber . '/image';
+
+    $header = array(
+        'Content-Type: application/json',
+        'Accept: application/json',
+        'Authorization: b90ecb77fe00ee07f61c22dca5036b93&2d977e9513aaf4df876c3c4b9e1874ac'
+    );
+
+    $restUrl = $IMBaseUrl . $restPostfix;
+
+    $response = CallAPI('GET', $restUrl, $header);
+    return json_decode($response, true);
+}
+
 function logErrorMessage($input, $response){
     $log = array();
     $log['item number'] = $input['ItemNumber'];
@@ -43,6 +61,7 @@ function logErrorMessage($input, $response){
     $log['date'] = $now->format('Y-m-d H:i:s');
     file_put_contents('post.log', json_encode($log) . PHP_EOL, FILE_APPEND);
 }
+
 // get rosewill product list from newegg
 $rwProductList = getRwProductList();
 if(!empty($rwProductList)) {
@@ -52,9 +71,12 @@ if(!empty($rwProductList)) {
         $input['action'] = 'baseinfo';
         $productInfo = getProductInfo($input);
         if (!empty($productInfo)) {
+            $imagesArray = getImagesFromIM($input['ItemNumber']);
+            $productInfo['Images'] = $imagesArray['Images'];
+
             $response = postProductInfoToDev($productInfo);
             if($response['message'] == 'Success'){
-                echo 'Success' . PHP_EOL;
+                echo 'Item Number: ' . $input['ItemNumber'] . ' post product info success' . PHP_EOL;
             }
             else{
                 echo 'Error' . PHP_EOL;
